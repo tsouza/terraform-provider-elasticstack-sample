6c86b9735d486be5e26b92483287607518a723c5
Thiago Souza <thiago@elastic.co>
2021-04-29 18:20:02 +0100

    adding elasticstack provider

---
+++ elasticstack.tf
@@ -0,0 +1,5 @@
+provider "elasticstack" {
+    elasticsearch_url = ec_deployment.onweek_demo.elasticsearch[0].https_endpoint
+    username = ec_deployment.onweek_demo.username
+    password = ec_deployment.onweek_demo.password
+}
