2fc949557a96b42efb7747aec1ed6a26d0577113
Thiago Souza <thiago@elastic.co>
2021-04-29 16:40:31 +0100

    update cluster name and zone count

--- ec_deployment.tf
+++ ec_deployment.tf
@@ -4,6 +4,7 @@ data "ec_stack" "latest" {
 }
 
 resource "ec_deployment" "onweek_demo" {
+  name                   = "onweek_demo"
   region                 = "us-east-1"
   version                = data.ec_stack.latest.version
   deployment_template_id = "aws-io-optimized-v2"
@@ -11,11 +12,13 @@ resource "ec_deployment" "onweek_demo" {
     topology {
       id   = "hot_content"
       size = "4g"
+      zone_count = 1
     }
   }
   kibana {
     topology {
       size = "1g"
+      zone_count = 1
     }
   }
 }
