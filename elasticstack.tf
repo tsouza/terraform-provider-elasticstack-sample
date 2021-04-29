e37396409526ca1ee48e2a1465a22721c996e475
Thiago Souza <thiago@elastic.co>
2021-04-29 18:42:17 +0100

    delete extra file

--- elasticstack.tf
+++
@@ -1,5 +0,0 @@
-provider "elasticstack" {
-  elasticsearch_url = ec_deployment.onweek_demo.elasticsearch[0].https_endpoint
-  username          = ec_deployment.onweek_demo.username
-  password          = ec_deployment.onweek_demo.password
-}
