d3e4e434ba1f422833c0a2ef7279e49c73721844
Steve Ross <steven.ross@elastic.co>
2021-04-29 20:24:22 -0400

    add fleet resource

--- endpoint.tf
+++ endpoint.tf
@@ -1,9 +1,9 @@
-resource "aws_lightsail_static_ip" "onweek_windows_test_ip" {
-  name = "onweek_windows_test_ip"
+resource "elasticstack_fleet_agent_policy" "endpoint_1" {
+  name = "onweek-endpoint-1"
 }
 
-resource "aws_lightsail_instance" "onweek_windows_test" {
-  name              = "onweek_windows_test"
+resource "aws_lightsail_instance" "onweek_windows_test_1" {
+  name              = "onweek_windows_test_1"
   availability_zone = "us-east-1a"
   blueprint_id      = "windows_server_2019"
   bundle_id         = "micro_win_2_0"
@@ -13,33 +13,18 @@ resource "aws_lightsail_instance" "onweek_windows_test" {
 $ProgressPreference = 'SilentlyContinue'
 Set-PSDebug -Trace 1
 Set-Location -Path "C:\Users\Administrator\Desktop"
+Add-MpPreference -ExclusionPath "C:\Users\Administrator\Desktop"
 
 Invoke-WebRequest -Uri "https://artifacts.elastic.co/downloads/beats/elastic-agent/elastic-agent-7.12.1-windows-x86_64.zip" -OutFile "agent.zip"
 Expand-Archive -Path "agent.zip" -DestinationPath "C:\Users\Administrator\Desktop\agent"
+Start-Process -Wait -FilePath "C:\Users\Administrator\Desktop\agent\elastic-agent-7.12.1-windows-x86_64\elastic-agent.exe" -ArgumentList "install","-f","--kibana-url=${data.terraform_remote_state.ess.outputs.ess_onweek_test_1_kibana_url}","--enrollment-token=${elasticstack_fleet_agent_policy.endpoint_1.enrollment_secret}"
 
-Start-Sleep -Seconds 10
+Start-Sleep -Seconds 120
 
-Add-MpPreference -ExclusionPath "C:\Users\Administrators\Desktop"
 Invoke-WebRequest -Uri "https://github.com/gentilkiwi/mimikatz/releases/download/2.2.0-20200918-fix/mimikatz_trunk.zip" -OutFile "mimikatz_trunk.zip"
 Expand-Archive -Path "mimikatz_trunk.zip" -DestinationPath "C:\Users\Administrator\Desktop\mimikatz"
 </powershell>
 EOF
-}
 
-resource "aws_lightsail_instance_public_ports" "onweek_test" {
-  instance_name = aws_lightsail_instance.onweek_windows_test.name
 
-  port_info {
-    protocol  = "tcp"
-    from_port = 3389
-    to_port   = 3389
-  }
-}
-resource "aws_lightsail_static_ip_attachment" "onweek_test" {
-  static_ip_name = aws_lightsail_static_ip.onweek_windows_test_ip.id
-  instance_name  = aws_lightsail_instance.onweek_windows_test.id
-}
-
-output "windows_ip" {
-  value = aws_lightsail_static_ip.onweek_windows_test_ip.ip_address
 }
